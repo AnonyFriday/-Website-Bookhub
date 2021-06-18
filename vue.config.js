81c7e3875c261c2d9f83342c5ebdf44ddbcad6ee
Vu Kim Duy <duyvukim752000@gmail.com>
2021-06-18 13:23:02 +1000

    [RECONFIGURE DEPLOYMENT]

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === "production" ? "/" + process.env.CI_PROJECT_NAME + "/" : "/",
+	publicPath: process.env.NODE_ENV === "production" ? "/Bookhub" : "/",
 };
